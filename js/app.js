d3b1e245345c29f3ac48b59596972d1e1332babc
Niveen Al Smadi <niveensmadi33@gmail.com>
2021-02-24 16:25:14 +0200

    added function

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ var small='s';
 var medium='m';
 var lovecake=prompt('Do you love CupCake??');
 var cakeFlavor=prompt('what is our only flavor ?');
+var number=prompt('how mutch pieces  you want ??');
 
 //if(cakeFlavor.toLocaleLowerCase()==='redvelvet'){ console.log('perfect flavor');}else{console.log('cool !!');}
 while(flavor!= cakeFlavor){
@@ -19,23 +20,34 @@ if(cakeSize.toLocaleLowerCase()===small)
 { console.log('you are in diet');}
 else if (cakeSize.toLocaleLowerCase()===medium){
     console.log('the best size');
-}else{
+  }else{
     alert('sorry Wrong Size!!');
-}
- var cakeprice=2.5;
+  }
+
+  
+    var cakeprice=2.5;
+    function price(cupcakes){
+     var totalprice= 2.5*number ; 
+     console.log(totalprice);
+ }
+ 
+ function your_order(order){
+     var txt=order;
+     console.log(txt);
+ } 
+
+ 
+ 
  var element=document.getElementById('price');
  element.innerText="ALways back"
  var element=document.getElementById('size');
  element.innerText="YOUR SIZE IS: " +cakeSize
  confirm( "Have Fun");
 
- function your_order(order){
-     var txt=order+"2.5jd";
-     console.log(txt);
-
- }
-
  your_order(cakeFlavor);
+ price(number);
+
+
 
 
 
