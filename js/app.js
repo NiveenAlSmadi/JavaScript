33602bc5885e46ed1c009beaf8e318310218af8a
Niveen Al Smadi <niveensmadi33@gmail.com>
2021-02-24 15:26:53 +0200

    added function

--- js/app.js
+++ js/app.js
@@ -3,16 +3,18 @@
 // make actions 
 
 
-
+var flavor="redvelvet";
 var small='s';
 var medium='m';
-var cakeFlavor=prompt('choose your flavor ?');
-var cakeSize=prompt( 'what size do you prefer??');
+var lovecake=prompt('Do you love CupCake??');
+var cakeFlavor=prompt('what is our only flavor ?');
 
-if(cakeFlavor.toLocaleLowerCase()==='redvelvet'){
-    console.log('perfect flavor');
- }else{
-    console.log('cool !!');}
+//if(cakeFlavor.toLocaleLowerCase()==='redvelvet'){ console.log('perfect flavor');}else{console.log('cool !!');}
+while(flavor!= cakeFlavor){
+    cakeFlavor=prompt('not correct please try again ?');
+}
+alert('you get it ');
+var cakeSize=prompt( 'what size do you prefer??');
 if(cakeSize.toLocaleLowerCase()===small)
 { console.log('you are in diet');}
 else if (cakeSize.toLocaleLowerCase()===medium){
@@ -22,10 +24,18 @@ else if (cakeSize.toLocaleLowerCase()===medium){
 }
  var cakeprice=2.5;
  var element=document.getElementById('price');
- element.innerText="you will pay to medium and small just a  :"+cakeprice
+ element.innerText="ALways back"
  var element=document.getElementById('size');
  element.innerText="YOUR SIZE IS: " +cakeSize
- confirm( "GOOD BAY ");
+ confirm( "Have Fun");
+
+ function your_order(order){
+     var txt=order+"2.5jd";
+     console.log(txt);
+
+ }
+
+ your_order(cakeFlavor);
 
 
 
